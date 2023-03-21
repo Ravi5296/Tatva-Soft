b82aea37d037cf65af7a1c7ebbf3a60122bec9ed
Aajugiya Ravi <aajugiyaravi@gmail.com>
2023-03-21 18:36:51 +0530

    new file:   Book-Store/src/pages/Product Page/ProductData.js 	modified:   Book-Store/src/pages/Product Page/ProductPage.jsx

---
+++ Book-Store/src/pages/Product Page/ProductData.js
@@ -0,0 +1,29 @@
+export const columns = [
+  { id: "id", label: "ID" },
+  { id: "title", label: "Title"},
+  { id: "price", label: "Price"},
+  { id: "query", label: "Query"},
+];
+
+export const bookData = [
+  {id : "1", title: "ISSM Followup", price: 2000, query: 'Teste'},
+  {id : "2", title: "ISSM Followup", price: 2000, query: 'Teste'},
+  {id : "3", title: "ISSM Followup", price: 2000, query: 'Teste'},
+  {id : "4", title: "ISSM Followup", price: 2000, query: 'Teste'},
+  {id : "5", title: "ISSM Followup", price: 2000, query: 'Teste'},
+  {id : "6", title: "ISSM Followup", price: 2000, query: 'Teste'},
+  {id : "7", title: "ISSM Followup", price: 2000, query: 'Teste'},
+  {id : "8", title: "ISSM Followup", price: 2000, query: 'Teste'},
+  {id : "9", title: "ISSM Followup", price: 2000, query: 'Teste'},
+  {id : "10", title: "title1", price: 2000, query: 'Teste'},
+  {id : "11", title: "title1", price: 2000, query: 'Teste'},
+  {id : "12", title: "title1", price: 2000, query: 'Teste'},
+  {id : "13", title: "title1", price: 2000, query: 'Teste'},
+  {id : "14", title: "title1", price: 2000, query: 'Teste'},
+  {id : "15", title: "title1", price: 2000, query: 'Teste'},
+  {id : "16", title: "title1", price: 2000, query: 'Teste'},
+  {id : "17", title: "title1", price: 2000, query: 'Teste'},
+  {id : "18", title: "title1", price: 2000, query: 'Teste'},
+  {id : "19", title: "title1", price: 2000, query: 'Teste'},
+  {id : "20", title: "title1", price: 2000, query: 'Teste'}
+];
--- Book-Store/src/pages/Product Page/ProductPage.jsx
+++ Book-Store/src/pages/Product Page/ProductPage.jsx
@@ -1,11 +1,71 @@
+import { TextField, Box, Button, TableContainer, Table, TableCell, TableHead, TableRow, TableBody } from '@mui/material'
 import React from 'react'
 import Heading from '../../assets/components/Heading'
+import { columns, bookData } from './ProductData'
 
 function ProductPage() {
+
+
   return (
-    <div>
+    <Box sx={{ width: '80%', m: 'auto' }} >
       <Heading title='Product Page' />
-    </div>
+      <Box sx={{ mx: '10px', display: 'flex', justifyContent: 'right', gap: '10px' }}>
+        <TextField
+          id="search"
+          placeholder='What are you looking for..'
+          InputProps={{ sx: { width: '300px', backgroundColor: 'white' } }}
+          size='small'
+        />
+        <Button variant='contained' >Add Product</Button>
+      </Box>
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell
+                  key={column.id}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {bookData.map((row) => (
+              <TableRow key={row.id}>
+                <TableCell>{row.id}</TableCell>
+                <TableCell>{row.title}</TableCell>
+                <TableCell>{row.price}</TableCell>
+                <TableCell>{row.query}</TableCell>
+                <TableCell>
+                  <Box sx={{display:'flex', gap:'10px', justifyContent:'right', }}>
+                    <Button
+                      type="button"
+                      className="green-btn btn"
+                      variant="outlined"
+                      color="primary"
+                      sx={{width:'80px', height:'30px'}}
+                    >
+                      Edit
+                    </Button>
+                    <Button
+                      type="button"
+                      variant='outlined'
+                      className="btn pink-btn"
+                      color="primary"
+                      sx={{width:'80px', height:'30px'}}
+                    >
+                      Delete
+                    </Button>
+                  </Box>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
   )
 }
 
